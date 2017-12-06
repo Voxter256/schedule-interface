8e1aa13a7ab702bd43ce0d27ffdbd322472f5e32
John Elgin <voxter.256@gmail.com>
2017-12-06 12:31:44 -0500

    fixed dateFormat to match fixed format when scraping

--- app/Shift.php
+++ app/Shift.php
@@ -10,7 +10,7 @@ class Shift extends Model
 
     protected $dates = ["shift_date"];
 
-    protected $dateFormat = 'Y-m-d H:i:s.u';
+    // protected $dateFormat = 'Y-m-d H:i:s';
 
     public function service(){
         return $this->belongsTo('App\Service');
@@ -18,5 +18,14 @@ class Shift extends Model
 
     public function physician(){
         return $this->belongsTo('App\Physician');
+    }
+
+    public function day_shift(){
+        $this_shift = Shift::whereDate('shift_date',  $this->shift_date)
+        ->where('physician_id', $this->physician_id)
+        ->whereHas('service', function ($query){
+            $query->where('is_call', False);
+        })->first();
+        return $this_shift;
     }
 }
